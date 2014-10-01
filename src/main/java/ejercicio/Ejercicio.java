297515dd7c6ea6620c30fe9d095ef2996e8862a1
Leire <leirehe@gmail.com>
2014-10-01 10:02:41 +0200

    release 1

--- src/main/java/ejercicio/Ejercicio.java
+++ src/main/java/ejercicio/Ejercicio.java
@@ -9,12 +9,16 @@ public class Ejercicio {
     public void develop1() {
 
     }
-    
+
     public void develop2() {
 
     }
-    
+
     public void develop3() {
+
+    }
+
+    public void release1() {
 
     }
 
